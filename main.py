121aa31cbab91ce756a43806704550f44917da98
kmahoney40 <kmahoney40@hotmail.com>
2019-07-07 20:56:46 -0700

    progress

--- main.py
+++ main.py
@@ -3,23 +3,63 @@ import sys
 import time
 import datetime
 
-import piplates.RELAYplate as RELAY
+#import piplates.RELAYplate as RELAY
 
 
 if __name__ == '__main__':
 
-    src = curses.iniscr()
-    src.noecho()
-    src.nodelay(1)
+    import time
+    import datetime
+
+    scr = curses.initscr()
+    curses.cbreak()
+    curses.noecho()
+    scr.keypad(1)
+    scr.nodelay(1)
 
     keepGoing = True
-    
+    count = 0
+
+    startTime = 615
+    # integar division in Python 2.x
+    stHour = startTime / 100
+    stMin  = startTime - stHour * 100
+    stMins = stMin + stHour * 60  
+
     while keepGoing:
-        dtm = datetime.datetime.now()
+        dtn = datetime.datetime.now()
         tm = time.time()
 
-        src.addstr(0, 0, "dtm: " + str(dtm))
-        src.addstr(1, 0, "tm:  " + str(tm))
+        scr.addstr(0, 0, "dtn: " + str(dtn))
+        scr.addstr(1, 0, "tm:  " + str(tm))
+
+        #dtNow is local 24hour time
+        dtNow = datetime.datetime.now()
+        dtNowHour = dtNow.hour
+        dtNowMinute  = dtNow.minute
+        scr.addstr(3, 0, "dtNow: " + str(dtNow))
+        scr.addstr(4, 0, "dtNowHour: " + str(dtNowHour))
+        scr.addstr(5, 0, "dtNowMinute: " + str(dtNowMinute))
+        dtNowMinute += dtNowHour * 60
+        scr.addstr(6, 0, "dtNowMinute-tot: " + str(dtNowMinute))
+        
+
+    
+        scr.addstr(7, 0, "startTime: " + str(startTime))
+        scr.addstr(8, 0, "stHour: " + str(stHour))
+        scr.addstr(9, 0, "stMin: " + str(stMin))
+        scr.addstr(10, 0, "stMins: " + str(stMins))
+       
+        
+
+ 
+        scr.refresh()
+        time.sleep(1)
+        count += 1
+        if count > 10:
+            keepGoing = False
     # while keepGoing
+
+    curses.endwin()
 
 # if __name__
