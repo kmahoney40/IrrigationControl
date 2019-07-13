824edfe9a125e7176ed502396c56d8369b753169
kmahoney40 <kmahoney40@hotmail.com>
2019-07-12 20:37:09 -0700

    timer loop working:

--- main.py
+++ main.py
@@ -21,16 +21,18 @@ if __name__ == '__main__':
     keepGoing = True
     count = 0
 
-    startTime = 1289
+    startTime = 2032
+
+
     # integar division in Python 2.x
     stHour = startTime / 100
     stMin  = startTime - stHour * 100
     stMins = stMin + stHour * 60
 
-    runTimes = [  [10, 0, 2, 0, 10, 0, 10],
-                    [0, 30, 0, 0, 30, 0, 0],
-                    [5, 5, 2, 5, 5, 5, 5],
-                    [0, 0, 0, 0, 0, 0, 0],
+    runTimes = [  [10, 0, 2, 2,   2, 0, 10],
+                    [0, 30, 0, 0, 1, 0, 0],
+                    [5, 5, 2, 2,  0, 0, 5],
+                    [0, 0, 0, 0,  1, 1, 0],
                     [0, 0, 0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0, 0, 0]]
@@ -52,8 +54,8 @@ if __name__ == '__main__':
         scr.addstr(3, 0, "dtNow: " + str(dtNow))
         scr.addstr(4, 0, "dtNowHour: " + str(dtNowHour))
         scr.addstr(5, 0, "dtNowMinute: " + str(dtNowMinute))
-        dtNowMinute += dtNowHour * 60
-        scr.addstr(6, 0, "dtNowMinute-tot: " + str(dtNowMinute))
+        dtNowMinute += dtNowHour * 60 - stMins
+        scr.addstr(6, 0, "dtNowMinute-tot: " + str(dtNowMinute) + " ")
 
 
 
@@ -63,25 +65,55 @@ if __name__ == '__main__':
         scr.addstr(10, 0, "stMins: " + str(stMins))
         scr.addstr(11, 0, "dtDay: " + str(dtDay))
 
-        scr.addstr(12, 0, str(runTimes[1][1]))
-        scr.addstr(13, 0, str(runTimes[2][6]))
+        scr.addstr(12, 0, str(runTimes[0][dtDay]))
+        scr.addstr(13, 0, str(runTimes[1][dtDay]))
 
         minsLt = 0
-        minsGt = stMins
-        for v in range(7):
-          minsLt += minsGt
-            minsGt += runTimes[v][dtDay]
-            if minsLt < dtNowMinute and dtNowMinute < minsGt:
-                scr.addstr(14 + v, 0, "Valve " + str(v) + " on")
-            else:
-                scr.addstr(14 + v, 0, "Valve " + str(v) + " off")
+        minsGt = 0
+        counter = [0, 0, 0, 0, 0, 0, 0]
+        if dtNowMinute >= 0: 
+            for v in range(7):
+                minsGt += runTimes[v][dtDay]
+                if v > 0:
+                    minsLt += runTimes[v-1][dtDay]
+                scr.addstr(21 + v, 0, "minsLt: " + str(minsLt) + " - minsGt: " + str(minsGt))
+                #minsLt  = minsGt
+                #minsGt += runTimes[v][dtDay]
+                counter[v] += 1
+                if minsLt <= dtNowMinute and dtNowMinute < minsGt:
+                    scr.addstr(14 + v, 0, "Valve" + str(v) + "  on")
+                else:
+                    scr.addstr(14 + v, 0, "Valve" + str(v) + " off")
+                    
+
+#                scr.addstr(22, 0, "minsLt: " + str(minsLt) +  " minsGt: " + str(minsGt) + " dtNowMinute: " + str(dtNowMinute))
+#                scr.addstr(23, 0, "v: " + str(v) + " dtDay: " + str(dtDay))
+#                while minsLt <= dtNowMinute and dtNowMinute < minsGt:
+#            minsLt += minsGt
+#            minsGt += runTimes[v][dtDay]
+
+                # update dtNowMinute here KMDB
+#                    dtNow = datetime.datetime.now()
+#                    dtDay = datetime.datetime.today().weekday()
+#                    dtNowHour = dtNow.hour
+#                    dtNowMinute  = dtNow.minute
+#                    if minsLt < dtNowMinute and dtNowMinute < minsGt:
+#                        scr.addstr(14 + v, 0, "Valve " + str(v) + " on")
+#                    else:
+#                        scr.addstr(14 + v, 0, "Valve " + str(v) + " off")
+#                    time.sleep(1)
 
 
         scr.refresh()
+    
+        c = scr.getch()
+        if c != curses.ERR:
+            keepGoing = False
+    
         time.sleep(1)
         count += 1
-        if count > 100:
-            keepGoing = False
+        #if count > 100:
+        #`    keepGoing = False
     # while keepGoing
 
     curses.endwin()
