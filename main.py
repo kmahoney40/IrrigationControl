279c13c2e5754c3ddb8cc2aa2136e66c7a3c2886
kmahoney40 <kmahoney40@hotmail.com>
2019-07-13 14:16:31 -0700

    cleanup

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     keepGoing = True
 
     # 24 hour loal time 600 = 6am, 1450 = 2:50 pm
-    startTime = 2107
+    startTime = 1359
     # Rows are valves, columns are days 0 - 6 for Mon - Sun, valve run times
     runTimes = [  [10, 0, 2, 2,   2, 0, 10],
                     [0, 30, 0, 0, 1, 0, 0],
@@ -47,26 +47,18 @@ if __name__ == '__main__':
         dtDay = datetime.datetime.today().weekday()
         dtNowHour = dtNow.hour
         dtNowMin  = dtNow.minute
-        scr.addstr(3, 0, "dtNow: " + str(dtNow))
-        scr.addstr(4, 0, "dtNowHour: " + str(dtNowHour))
-        scr.addstr(5, 0, "dtNowMin: " + str(dtNowMin))
         dtNowMin += dtNowHour * 60 - stMin
         # Clear extra characters for negative and 3 or 4 digit times
-        scr.addstr(6, 0, "dtNowMin-tot: " + str(dtNowMin) + "      ")
+        scr.addstr(6, 0, "dtNowMin: " + str(dtNowMin) + "      ")
 
 
 
         scr.addstr(7, 0, "startTime: " + str(startTime))
-        scr.addstr(8, 0, "stHour: " + str(stHour))
-        scr.addstr(9, 0, "stMin: " + str(stMin))
         scr.addstr(10, 0, "stMins: " + str(stMin))
         scr.addstr(11, 0, "dtDay: " + str(dtDay))
 
-        scr.addstr(12, 0, str(runTimes[0][dtDay]))
-        scr.addstr(13, 0, str(runTimes[1][dtDay]))
-
-        ltMin = 0
         gtMin = 0
+        ltMin = 0
         if dtNowMin >= 0: 
             for v in range(len(runTimes)):
                 gtMin += runTimes[v][dtDay]
