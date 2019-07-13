b769fa8903d4647b3a3d98315dccf2d0dbc088d6
kmahoney40 <kmahoney40@hotmail.com>
2019-07-12 21:11:37 -0700

    cleanup

--- main.py
+++ main.py
@@ -19,16 +19,10 @@ if __name__ == '__main__':
     scr.nodelay(1)
 
     keepGoing = True
-    count = 0
 
-    startTime = 2032
-
-
-    # integar division in Python 2.x
-    stHour = startTime / 100
-    stMin  = startTime - stHour * 100
-    stMins = stMin + stHour * 60
-
+    # 24 hour loal time 600 = 6am, 1450 = 2:50 pm
+    startTime = 2107
+    # Rows are valves, columns are days 0 - 6 for Mon - Sun, valve run times
     runTimes = [  [10, 0, 2, 2,   2, 0, 10],
                     [0, 30, 0, 0, 1, 0, 0],
                     [5, 5, 2, 2,  0, 0, 5],
@@ -36,73 +30,53 @@ if __name__ == '__main__':
                     [0, 0, 0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0, 0, 0]]
-    valvePos = ["Off", "Off", "Off", "Off", "Off", "Off", "Off"]
+
+
+    # integar division in Python 2.x
+    stHour = startTime / 100
+    stMin  = startTime - stHour * 100
+    stMin  = stMin + stHour * 60
 
     while keepGoing:
         dtn = datetime.datetime.now()
-        tm = time.time()
 
         scr.addstr(0, 0, "dtn: " + str(dtn))
-        scr.addstr(1, 0, "tm:  " + str(tm))
 
-
-               #dtNow is local 24hour time
+        #dtNow is local 24hour time
         dtNow = datetime.datetime.now()
         dtDay = datetime.datetime.today().weekday()
         dtNowHour = dtNow.hour
-        dtNowMinute  = dtNow.minute
+        dtNowMin  = dtNow.minute
         scr.addstr(3, 0, "dtNow: " + str(dtNow))
         scr.addstr(4, 0, "dtNowHour: " + str(dtNowHour))
-        scr.addstr(5, 0, "dtNowMinute: " + str(dtNowMinute))
-        dtNowMinute += dtNowHour * 60 - stMins
-        scr.addstr(6, 0, "dtNowMinute-tot: " + str(dtNowMinute) + " ")
+        scr.addstr(5, 0, "dtNowMin: " + str(dtNowMin))
+        dtNowMin += dtNowHour * 60 - stMin
+        # Clear extra characters for negative and 3 or 4 digit times
+        scr.addstr(6, 0, "dtNowMin-tot: " + str(dtNowMin) + "      ")
 
 
 
         scr.addstr(7, 0, "startTime: " + str(startTime))
         scr.addstr(8, 0, "stHour: " + str(stHour))
         scr.addstr(9, 0, "stMin: " + str(stMin))
-        scr.addstr(10, 0, "stMins: " + str(stMins))
+        scr.addstr(10, 0, "stMins: " + str(stMin))
         scr.addstr(11, 0, "dtDay: " + str(dtDay))
 
         scr.addstr(12, 0, str(runTimes[0][dtDay]))
         scr.addstr(13, 0, str(runTimes[1][dtDay]))
 
-        minsLt = 0
-        minsGt = 0
-        counter = [0, 0, 0, 0, 0, 0, 0]
-        if dtNowMinute >= 0: 
-            for v in range(7):
-                minsGt += runTimes[v][dtDay]
+        ltMin = 0
+        gtMin = 0
+        if dtNowMin >= 0: 
+            for v in range(len(runTimes)):
+                gtMin += runTimes[v][dtDay]
                 if v > 0:
-                    minsLt += runTimes[v-1][dtDay]
-                scr.addstr(21 + v, 0, "minsLt: " + str(minsLt) + " - minsGt: " + str(minsGt))
-                #minsLt  = minsGt
-                #minsGt += runTimes[v][dtDay]
-                counter[v] += 1
-                if minsLt <= dtNowMinute and dtNowMinute < minsGt:
+                    ltMin += runTimes[v-1][dtDay]
+                scr.addstr(21 + v, 0, "ltMin: " + str(ltMin) + " - gtMin: " + str(gtMin))
+                if ltMin <= dtNowMin and dtNowMin < gtMin:
                     scr.addstr(14 + v, 0, "Valve" + str(v) + "  on")
                 else:
                     scr.addstr(14 + v, 0, "Valve" + str(v) + " off")
-                    
-
-#                scr.addstr(22, 0, "minsLt: " + str(minsLt) +  " minsGt: " + str(minsGt) + " dtNowMinute: " + str(dtNowMinute))
-#                scr.addstr(23, 0, "v: " + str(v) + " dtDay: " + str(dtDay))
-#                while minsLt <= dtNowMinute and dtNowMinute < minsGt:
-#            minsLt += minsGt
-#            minsGt += runTimes[v][dtDay]
-
-                # update dtNowMinute here KMDB
-#                    dtNow = datetime.datetime.now()
-#                    dtDay = datetime.datetime.today().weekday()
-#                    dtNowHour = dtNow.hour
-#                    dtNowMinute  = dtNow.minute
-#                    if minsLt < dtNowMinute and dtNowMinute < minsGt:
-#                        scr.addstr(14 + v, 0, "Valve " + str(v) + " on")
-#                    else:
-#                        scr.addstr(14 + v, 0, "Valve " + str(v) + " off")
-#                    time.sleep(1)
-
 
         scr.refresh()
     
@@ -111,9 +85,7 @@ if __name__ == '__main__':
             keepGoing = False
     
         time.sleep(1)
-        count += 1
-        #if count > 100:
-        #`    keepGoing = False
+
     # while keepGoing
 
     curses.endwin()
